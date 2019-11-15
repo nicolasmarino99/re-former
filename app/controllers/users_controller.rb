d2c7540cc7fdbe7fe290c977c525d21f063e78e0
Bruna <brunagenz92@gmail.com>
2019-11-15 18:45:53 -0400

    create update and show routes; create #show and #update methods; create edit and show views;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,28 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user)
     else
       render :new
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated."
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
   end
 
   private
