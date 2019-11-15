922ca2e1777064bec1493406a6853579549998f1
Bruna <brunagenz92@gmail.com>
2019-11-15 17:04:08 -0400

    create form_for @user; create #edit method for user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    
     @user = User.new(user_params)
 
     if @user.save
@@ -13,9 +13,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    params.permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
